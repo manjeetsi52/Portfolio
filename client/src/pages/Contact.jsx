189112f608b90b159a833b680242e9dde0f9d745
manjeetsi52 <manjeetsinghyk@gmail.om>
2026-01-12 19:22:51 +0530

    added notificaiton

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,31 +3,32 @@ import { IoLogoLinkedin } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import { Button } from "@mui/material";
 import { FaLongArrowAltRight } from "react-icons/fa";
-
+import { Toaster, toast } from "sonner";
 import { MarqueeText } from "../components/slider/Slider.jsx";
 import { useState } from "react";
-
+const initialFormData = {
+  name: "",
+  email: "",
+  message: "",
+};
 export const Contact = () => {
-    const [formData,setFormData] = useState({
-        name:'',
-        email:'',
-        message:''
-    })
-    const handleClick = (e)=>{
-        const {name,value} = e.target
-        setFormData((prev)=>{
-            return {...prev,[name]:value}
-        })
-    }
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        console.log(formData)
-        // send data to backend and send email to me
-    }
+  const [formData, setFormData] = useState(initialFormData);
+  const handleClick = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    toast.success("Message sent successfully");
+    setFormData(initialFormData);
+    // send data to backend and send email to me
+  };
 
   return (
     <section className="min-h-screen contain text-white font-poppins flex flex-col justify-between">
-      
       {/* Top Marquee */}
       <div className="py-10">
         <MarqueeText text="SAY HELLO" />
@@ -35,15 +36,17 @@ export const Contact = () => {
 
       {/* Main Content */}
       <div className="md:contain grid grid-cols-1 md:grid-cols-2 gap-10 items-center relative ">
-        
         {/* Left */}
         <div className="flex flex-col gap-6 justify-center">
           <h1 className="text-5xl md:text-6xl font-semibold tracking-wide bg-gradient-to-bl from-violet-100 via-fuchsia-500 to-fuchsia-700 bg-clip-text text-transparent text-center md:text-left">
             Manjeet Singh
           </h1>
 
-          <div className="flex gap-5 justify-center md:justify-baseline">
-            <a href="https://www.linkedin.com/in/manjeet-singh-5987b027b/" target="_blank">
+          <div className="flex gap-5 justify-center md:justify-start">
+            <a
+              href="https://www.linkedin.com/in/manjeet-singh-5987b027b/"
+              target="_blank"
+            >
               <IoLogoLinkedin className="md:text-3xl text-2xl text-zinc-400 hover:text-fuchsia-500 transition" />
             </a>
             <a href="https://github.com/manjeetsi52" target="_blank">
@@ -58,19 +61,42 @@ export const Contact = () => {
             alt="image"
             className="absolute w-32 top-80 left-120 animate-[slowBounce_3s_ease-in-out_infinite] hidden md:visible"
           />
-         
         </div>
 
         {/* Right */}
         <div className="flex justify-center">
-          <form onSubmit={handleSubmit} className="w-full max-w-md p-5 md:p-10 rounded-xl md:rounded-2xl border border-zinc-700 flex flex-col gap-6 bg-black/40 backdrop-blur hover:border-zinc-500 transition">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full max-w-md p-5 md:p-10 rounded-xl md:rounded-2xl border border-zinc-700 flex flex-col gap-6 bg-black/40 backdrop-blur hover:border-zinc-500 transition"
+          >
             {/* <h2 className="text-2xl font-semibold text-center">
               Get in touch
             </h2> */}
 
-            <input type="text" placeholder="Your name" className="input" value={formData.name} onChange={handleClick}/>
-            <input type="email" placeholder="Your email" className="input" value={formData.email} onChange={handleClick}/>
-            <textarea placeholder="Your message" rows="4" className="input resize-none" value={formData.message} onChange={handleClick}/>
+            <input
+              type="text"
+              placeholder="Your name"
+              name="name"
+              className="input"
+              value={formData.name}
+              onChange={handleClick}
+            />
+            <input
+              type="email"
+              placeholder="Your email"
+              name="email"
+              className="input"
+              value={formData.email}
+              onChange={handleClick}
+            />
+            <textarea
+              placeholder="Your message"
+              rows="4"
+              name="message"
+              className="input resize-none"
+              value={formData.message}
+              onChange={handleClick}
+            />
 
             <Button
               variant="outlined"
@@ -84,9 +110,8 @@ export const Contact = () => {
             </Button>
           </form>
         </div>
-
       </div>
-
+      <Toaster position="top-center" richColors />
       {/* Bottom Marquee */}
       <div className="py-10">
         <MarqueeText text="SAY HELLO" />
